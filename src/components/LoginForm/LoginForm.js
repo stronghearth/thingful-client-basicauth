c58dc771d2276e980ee27bfc7906687185a0b766
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-02-07 16:19:12 -0500

    added JWT functionality

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import TokenService from '../../services/token-service'
+import AuthApiService from '../../services/auth-api-service'
 import { Button, Input } from '../Utils/Utils'
 
 export default class LoginForm extends Component {
@@ -9,7 +10,27 @@ export default class LoginForm extends Component {
 
   state = { error: null }
 
-  handleSubmitBasicAuth = ev => {
+  handleSubmitJWTAuth = e => {
+    e.preventDevault()
+    this.setState({ error: null})
+    const { user_name, password} = ev.target
+
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+    .then(res => {
+      user_name.value = ''
+      password.value= ''
+      TokenService.saveAuthToken(res.authToken)
+      this.props.onLoginSuccess()
+    })
+    .catch(res => {
+      this.setState({error: res.error})
+    })
+  }
+
+  {/*handleSubmitBasicAuth = ev => {
     ev.preventDefault()
     const { user_name, password } = ev.target
 
@@ -20,14 +41,14 @@ export default class LoginForm extends Component {
     user_name.value = ''
     password.value = ''
     this.props.onLoginSuccess()
-  }
+  }*/}
 
   render() {
     const { error } = this.state
     return (
       <form
         className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
+        onSubmit={this.handleSubmitJWTAuth}
       >
         <div role='alert'>
           {error && <p className='red'>{error}</p>}
